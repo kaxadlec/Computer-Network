d35a1d76f4206606812fc5ae7597931ae8b784e5
kaxadlec <kaxadlec@gmail.com>
2023-02-20 00:44:14 +0900

    TCP_Product v3

--- ProductClient.java
+++ ProductClient.java
@@ -1,31 +1,40 @@
 package HW;
 
+import javax.xml.crypto.Data;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 // ProductClient 클래스 선언
 public class ProductClient {
-    public void start() throws IOException {
+    public static void main(String[] args) {
+        Socket socket = null;
+        DataInputStream dis = null;
+        DataOutputStream dos = null;
+        Scanner scanner = null;
 
-        Socket socket = new Socket("localhost", 50001);
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-        System.out.println("[클라이언트] 서버에 연결됨");
 
-        Scanner scanner = new Scanner(System.in);
+        ProductClient productclient = new ProductClient();
+        try{
+            socket = new Socket("localhost", 50001);
+            dis = new DataInputStream(socket.getInputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
+            System.out.println("[클라이언트] 서버에 연결됨");
+            scanner = new Scanner(System.in);
 
-        list();
-    }
+            list();
 
-    public void stop() {
+
+        } catch (IOException e) {
+            System.out.println("입출력 예외발생");
+        }
         try {
             socket.close();
             scanner.close();
         } catch(Exception e) {}
         System.out.println("[클라이언트] 종료됨");
     }
-
 }
